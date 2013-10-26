c78d5b8a51c47b36a298e811d6255851b0f0ddb2
Drew Blaisdell <drew.blaisdell@gmail.com>
2013-10-25 17:12:09 -0700

    alarms work now

--- js/app.js
+++ js/app.js
@@ -62,6 +62,10 @@ app.models = {
 					app.models.price.data.last = app.models.price.data.now;
 				}
 
+				console.log(data.data.last_local.display);
+
+				console.log("Updated at "+ Date.now());
+
 				app.models.price.data.now = last_local;
 				app.gotPrice(app.models.price.data.now);
 			});
@@ -79,7 +83,19 @@ app.views = {
 		},
 
 		remove: function(id){
-			$('.delete[data-id="'+ id +'"]').parents('.alarm').remove();	
+			$('.alarm[data-id="'+ id +'"]').remove();	
+		},
+
+		activate: function(id){
+			$('.alarm[data-id="'+ id +'"]').addClass('active');
+		},
+
+		deactivate: function(id){
+			$('.alarm[data-id="'+ id +'"]').removeClass('active');			
+		},
+
+		deactivateAll: function(){
+			$('.alarm.active').removeClass('active');
 		}
 	},
 
@@ -87,18 +103,51 @@ app.views = {
 		display: function(price){
 			$('#price').text(price);
 		}
+	},
+
+	modal: {
+		display: function(price){
+			var compiledTemplate = Mustache.compile(app.templates.modal);
+			var templateOutput = compiledTemplate({ price: price });
+			$('body').append(templateOutput);
+			setTimeout(function(){
+				$('.curtain').addClass('active');
+			}, 1);
+		}
 	}
 };
 
 // controller
-app.updatePrice = setInterval(app.models.price.update, 10000);
+app.updatePrice = setInterval(app.models.price.update, 5000);
+
+app.showModal = function(price){
+	app.views.modal.display(price);
+};
+
+app.closeModal = function(){
+	$('.curtain').removeClass('active');
+	setTimeout(function(){
+		$('.curtain').remove();
+	}, 200);
+
+	app.stopAlarm();
+};
+
+app.soundAlarm = function(alarm){
+	app.showModal(alarm.price);
+	app.views.alarms.activate(alarm.id);
+	app.startAlarm();
+};
 
 app.startAlarm = function(){
 	app.alarm = setInterval(beep, 600);
 };
 
 app.stopAlarm = function(){
+	console.log("STOPPING ALARM");
 	clearInterval(app.alarm);
+	app.alarm = false;
+	app.views.alarms.deactivateAll();
 };
 
 app.gotPrice = function(price){
@@ -124,12 +173,16 @@ app.setType = function(){
 };
 
 app.clickDelete = function(){
-	var id = $(this).data('id');
+	var id = $(this).parents('.alarm').data('id');
 	app.models.alarms.delete(id);
 	app.views.alarms.remove(id);
 };
 
 app.checkAlarms = function(){
+	if(app.alarm){
+		return;
+	}
+
 	var alarms = app.models.alarms.get();
 	for(var i = 0, j = alarms.length; i < j; i++){
 		if(alarms[i]){
@@ -138,16 +191,23 @@ app.checkAlarms = function(){
 				currentPrice = parseFloat(app.models.price.get().value),
 				lastPrice = parseFloat(app.models.price.getLast().value);
 
-			console.log('price: '+ price +' current: '+ currentPrice +' last: '+ lastPrice);
-
-			if(price >= currentPrice && price < lastPrice){
-				// sound alarm
-				console.log("ALRM");
-			} else if (price <= currentPrice && price > lastPrice){
-				// sound alarm
-				console.log("ALRM");
+ 			if((price >= currentPrice && price < lastPrice) || (price <= currentPrice && price > lastPrice)){
+ 				app.runAlarm(alarm);
 			}
 		}
+	}
+};
+
+app.runAlarm = function(alarm){
+	if(alarm.type === 'alarm'){
+		app.soundAlarm(alarm);
+	} else if(alarm.type = 'one beep') {
+		app.views.alarms.activate(alarm.id);
+		beep();
+
+		setTimeout(function(){
+			app.views.alarms.deactivate(alarm.id);
+		}, 400);
 	}
 };
 
@@ -156,6 +216,12 @@ $(document).ready(function(){
 	app.models.price.update();
 
 	$('.set-alarm').click(app.setAlarm);
+	$('.price-threshold').keypress(function(event){
+		if(event.keyCode === 13){
+			app.setAlarm();
+		}
+	});
 	$('.alarm-option').click(app.setType);
 	$('.alarms').on('click', '.delete', app.clickDelete);
+	$('body').on('click', '.curtain', app.closeModal);
 });
--- js/templates.js
+++ js/templates.js
@@ -1,4 +1,6 @@
 var app = app || {};
 app.templates = app.templates || {};
 
-app.templates.alarm = '<div class="alarm"><div class="symbol-wrapper"><div class="symbol">{{type}}</div></div><div class="alarm-text">${{price}}</div><div class="delete button" data-id="{{id}}">delete</div></div>';
+app.templates.alarm = '<div class="alarm" data-id="{{id}}"><div class="symbol-wrapper"><div class="symbol">{{type}}</div></div><div class="alarm-text">${{price}}</div><div class="delete button">delete</div></div>';
+
+app.templates.modal = '<div class="curtain"><div class="modal"><header>Alarm Started</header><div class="inner">The price moved past your alarm set at ${{price}}.<div class="stop-alarm button">stop alarm</div></div></div></div>';
